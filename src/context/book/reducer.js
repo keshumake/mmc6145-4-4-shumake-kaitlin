cc4c1ee849dd957057b5ef28f606515291324e6b
Kaitlin Shumake <shumakek@ufl.edu>
2023-02-27 20:55:26 -0500

    amended reducer.js

--- src/context/book/reducer.js
+++ src/context/book/reducer.js
@@ -1,5 +1,25 @@
 // TODO: import actions and implement reducer for each action
 export default function reducer() {}
+import {
+  ADD_BOOK,
+  REMOVE_BOOK,
+  SEARCH_BOOKS
+} from './actions'
+
+export default function reducer(prevState, {action, payload}) {
+  const favBooks = prevState
+  switch(action) {
+    case ADD_BOOK:
+      return {...prevState, favBooks: favBooks + 1}
+    case REMOVE_BOOK:
+      return {...prevState, favBooks: favBooks - 1}
+    case SEARCH_BOOKS:
+      return {...prevState}
+    }
+    default:
+      return prevState
+  }
+}
 
 // This helper function stores the favoriteBook state in localStorage as a string
 function saveToLocalStorage(favBooks) {
